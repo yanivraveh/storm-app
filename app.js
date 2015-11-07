9cfebe3ea0059f603c496eb452693d1532f98a6e
yanivraveh <yanivraveh@gmail.com>
2015-11-07 23:47:29 +0200

    enabling google

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 app.set('views', './views');
 app.set('view engine', 'jade');
 
+
 var stormpathMiddleware = stormpath.init(app, {
   apiKeyFile: 'apiKey-3C1XCR5DD3YKXK0UPKRJPK4ZV.properties',
   application: 'https://api.stormpath.com/v1/applications/7ST6geHE2SaiVYYYVKRn4s',
@@ -27,6 +28,15 @@ app.get('/', function(req, res) {
 
 app.use('/profile',stormpath.loginRequired,require('./profile')());
 
+app.use(stormpath.init(app, {
+  enableGoogle: true,
+  social: {
+    google: {
+      clientId: '438370927701-vr2e6va4ljp5bjl5r4jro707lu3ume7v.apps.googleusercontent.com',
+      clientSecret: 'i_HoGP3ebOENhIX3OrbLRSvN',
+    },
+  },
+}));
 //app.listen(3000);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
